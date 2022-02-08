d92bf842eda88c5215249bb5edfa49b2d07e5d6a
福井陽大 <fukuiyota@fukuiakiradainoMacBook-Pro.local>
2022-02-08 17:23:31 +0900

    modify

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'トイレを探す'),
+      home: const MyHomePage(title: 'トイレを探す。'),
     );
   }
 }

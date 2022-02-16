838a11b589664943c7bc4b335206a7b80254de00
ken2403 <ken2403.pmtdg@gmail.com>
2022-02-16 20:15:41 +0900

    change API keys for security reason

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -11,7 +11,7 @@ import GoogleMaps  // Add this import for Google map
     GeneratedPluginRegistrant.register(with: self)
 
     // Add Kento Nishio's Google Maps API key
-    GMSServices.provideAPIKey("AIzaSyA_15QRxeFpcofg5hTQG12IQqk1Yyf2S7U")
+    GMSServices.provideAPIKey("AIzaSyCxfH-mghbFZVvsDb7gMafz2bxK8Thfwk4")
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }

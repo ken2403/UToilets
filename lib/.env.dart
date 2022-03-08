8341c7963d54085de4879a891370fd721eecb79b
ken2403 <ken2403.pmtdg@gmail.com>
2022-03-09 02:02:10 +0900

    modify paging

---
+++ lib/.env.dart
@@ -0,0 +1,1 @@
+const String googleAPIKey = 'AIzaSyBX5G9F4fN-NxZ3SmKxEQXT9SWXpMhqOlU';
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'pages/filters_page.dart';
-import './pages/map_page.dart';
-import './pages/search_page.dart';
+
 import './pages/homepage.dart';
 
 void main() {
@@ -16,29 +14,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // global変数(下の_filters)をこのmain.dartファイルで管理している。
-  // filter_pageでウォシュレットとかが選択されて保存が押されたらfilter_pageのwashlet変数だけでなくこのgloabal変数も変更するようにしている。
-  // この変更されたglobal変数を下のrouteからmap_pageへ送ることでフィルタリングされたトイレのみmap_page→map_widgetで表示されるようになっている。
-  Map<String, Object> _filters = {
-    'multipurpose': false,
-    'washlet': false,
-    'madeyear': 1900,
-    'recyclePaper': false,
-    'singlePaper': false,
-    'seatWarmer': false,
-    'isfiltered': false,
-  };
-
-  void _setFilters(Map<String, Object> filterData) {
-    setState(() {
-      _filters = filterData;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Search available toilet',
+      home: const HomePage(),
       theme: ThemeData(
         primarySwatch: Colors.lightBlue,
         accentColor: Colors.lightGreen,
@@ -54,17 +34,6 @@ class _MyAppState extends State<MyApp> {
               fontSize: 18,
             )),
       ),
-      initialRoute: '/',
-      routes: {
-        HomePage.routeName: (ctx) => const HomePage(),
-        SearchPage.routeName: (ctx) => const SearchPage(),
-        MapPage.routeNameFromSearch: (ctx) =>
-            MapPage(filters: _filters, title: '検索結果'),
-        MapPage.routeName: (ctx) => MapPage(filters: _filters, title: '地図から探す'),
-        // global変数(main.dart内の_filtersとそれを更新する関数をfilter_pageに引数として渡している。)
-        FilterPage.routeName: (ctx) =>
-            FilterPage(currentFilters: _filters, saveFilters: _setFilters),
-      },
     );
   }
 }

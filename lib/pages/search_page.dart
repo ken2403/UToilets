0e04fa62d4d425804e5d31be0a818ce2409efb9a
ken2403 <ken2403.pmtdg@gmail.com>
2022-03-10 00:20:39 +0900

    modify import

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,9 +1,6 @@
 import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import './map_page.dart';

3cdcc0ea32221f2f74ceb0072e211a9d9746e39c
ken2403 <ken2403.pmtdg@gmail.com>
2022-03-11 16:46:16 +0900

    make

--- lib/pages/mypage/setting_page.dart
+++ lib/pages/mypage/setting_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../.env.dart';
+import '../homepage.dart';
+import '../../widgets/appbar.dart';
 
 class SettingPage extends StatefulWidget {
   /*
@@ -14,15 +19,127 @@ class SettingPage extends StatefulWidget {
 }
 
 class _SettingPageState extends State<SettingPage> {
+  // set some constants
+  final radioText = <ChosenSex, String>{
+    ChosenSex.male: '男性',
+    ChosenSex.female: '女性',
+  };
+  // set some variables
+  ChosenSex _chosenSex = ChosenSex.male;
+
+  // function to change _chosenSex to selected sex
+  void _onRadioSelected(value) {
+    setState(() {
+      _chosenSex = value;
+    });
+  }
+
+  // load saved settings
+  Future<void> _loadSavedParams() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _chosenSex = prefs.getInt('sex') == 0 ? ChosenSex.male : ChosenSex.female;
+    });
+  }
+
+  // save setting
+  Future<void> _paramSaver() async {
+    final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+    final SharedPreferences prefs = await _prefs;
+    await prefs.setInt('sex', _chosenSex == ChosenSex.male ? 0 : 1);
+  }
+
   @override
   Widget build(BuildContext context) {
+    _loadSavedParams();
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: true,
-        title: const Text(SettingPage.title),
+      appBar: customAppbar(context, SettingPage.title),
+      body: Padding(
+        padding: pagePadding,
+        child: ListView(
+          children: <Widget>[
+            Card(
+              elevation: 0,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 3,
+                      child: Text(
+                        '性別を変更する',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    ),
+                    Radio(
+                      value: ChosenSex.male,
+                      groupValue: _chosenSex,
+                      activeColor: Theme.of(context).colorScheme.primary,
+                      onChanged: (value) => _onRadioSelected(value),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(right: 15),
+                      child: Text(
+                        radioText[ChosenSex.male]!,
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    ),
+                    Radio(
+                      value: ChosenSex.female,
+                      groupValue: _chosenSex,
+                      activeColor: Theme.of(context).colorScheme.primary,
+                      onChanged: (value) => _onRadioSelected(value),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(right: 15),
+                      child: Text(
+                        radioText[ChosenSex.female]!,
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              elevation: 0,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 3 * 2,
+                      child: Text(
+                        '位置情報の設定を開く',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    ),
+                    TextButton(
+                        // TODO:位置情報
+                        onPressed: () {},
+                        child: Text(
+                          '開く',
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ))
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
-      body: const Center(
-        child: Text(SettingPage.title),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        label: Text(
+          '保存する',
+          style: Theme.of(context).textTheme.button,
+        ),
+        onPressed: () {
+          _paramSaver();
+          Navigator.of(context).popUntil((route) => route.isFirst);
+        },
       ),
     );
   }

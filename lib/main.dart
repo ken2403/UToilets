ecf54a2dbe9e5e7d810e417deeb1e94b7b4d1113
ken2403 <ken2403.pmtdg@gmail.com>
2022-03-11 16:45:21 +0900

    Modify theme

--- lib/main.dart
+++ lib/main.dart
@@ -20,22 +20,40 @@ class _UToiletState extends State<UToilet> {
       title: 'Search available toilet',
       home: const HomePage(),
       theme: ThemeData(
+        unselectedWidgetColor: Colors.black54,
         colorScheme: ColorScheme.fromSwatch(
           primarySwatch: Colors.lightBlue,
         ).copyWith(
+          primary: Colors.lightBlue,
           secondary: Colors.lightGreen,
+          background: Colors.black87.withAlpha(100),
+          surface: Colors.white,
         ),
         canvasColor: const Color.fromRGBO(255, 254, 249, 1),
-        // フォント追加してみた。別にいらないかも
         fontFamily: 'ARIAL',
         textTheme: ThemeData.light().textTheme.copyWith(
-            headline6: const TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+              headline6: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              headline5: const TextStyle(
+                fontSize: 21,
+                fontWeight: FontWeight.bold,
+              ),
+              bodyText1: const TextStyle(
+                fontSize: 18,
+                color: Colors.black87,
+              ),
+              bodyText2: const TextStyle(
+                fontSize: 15,
+                color: Colors.black54,
+              ),
+              button: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
             ),
-            bodyText1: const TextStyle(
-              fontSize: 18,
-            )),
       ),
     );
   }

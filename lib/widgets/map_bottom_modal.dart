db4a4616f3d673cab02d91a7ab6d45a48974d709
ken2403 <ken2403.pmtdg@gmail.com>
2022-03-11 16:45:44 +0900

    modify comment

--- lib/widgets/map_bottom_modal.dart
+++ lib/widgets/map_bottom_modal.dart
@@ -6,7 +6,7 @@ class MapBottomModal extends StatelessWidget {
   */
   // constructor
   const MapBottomModal({Key? key}) : super(key: key);
-
+  // TODO：作る
   @override
   Widget build(BuildContext context) {
     return Container();

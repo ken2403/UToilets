ea5a7e5309438a1e49bd39a89d7aeca94213a60c
ken2403 <ken2403.pmtdg@gmail.com>
2022-03-12 09:51:23 +0900

    modif pagePadding

--- lib/.env.dart
+++ lib/.env.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 const String googleAPIKey = 'AIzaSyBX5G9F4fN-NxZ3SmKxEQXT9SWXpMhqOlU';
 
-const EdgeInsets pagePadding = EdgeInsets.fromLTRB(15, 25, 15, 15);
+const EdgeInsets pagePadding = EdgeInsets.fromLTRB(15, 20, 15, 15);
